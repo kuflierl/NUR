3a3a41c822e1d37afe32139ab5abd348c3e151e9
kuflierl <41301536+kuflierl@users.noreply.github.com>
2025-05-23 14:07:20 +0200

    feat: add 25.05 channel and implement metadata patching for old nixpkgs

--- pkgs/top-level/default.nix
+++ pkgs/top-level/default.nix
@@ -2,7 +2,40 @@
 let
   callPackageByName = name: extraArgs: callPackage (lib.pkgPathByName name) extraArgs;
 
-  callPackage = pkgs.lib.callPackageWith (pkgs // { customLib = lib; } // packages);
+  pkgsLibPatch = pkgs.lib // {
+    maintainers =
+      pkgs.lib.maintainers
+      // pkgs.lib.optionalAttrs (pkgs.lib.versionOlder pkgs.lib.version "25.05pre-git") {
+        kuflierl = {
+          email = "kuflierl@gmail.com";
+          github = "kuflierl";
+          githubId = 41301536;
+          name = "Kennet Flierl";
+        };
+      };
+    licenses =
+      pkgs.lib.licenses
+      // pkgs.lib.optionalAttrs (pkgs.lib.versionOlder pkgs.lib.version "25.05pre-git") {
+        llvm-exception = {
+          deprecated = false;
+          free = true;
+          fullName = "LLVM Exception";
+          redistributable = true;
+          shortName = "llvm-exception";
+          spdxId = "LLVM-exception";
+          url = "https://spdx.org/licenses/LLVM-exception.html";
+        };
+      };
+  };
+
+  callPackage = pkgs.lib.callPackageWith (
+    pkgs
+    // {
+      lib = pkgsLibPatch;
+      customLib = lib;
+    }
+    // packages
+  );
   packages = {
     intel-oneapi-dpcpp-cpp = callPackageByName "intel-oneapi-dpcpp-cpp" { };
     level-zero-1-19 = callPackageByName "level-zero-1-19" { };
